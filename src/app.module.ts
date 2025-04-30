f8140f241b9d530e19a952ffa0014cad2791badb
Veronica-Alfr <veagalves@gmail.com>
2025-04-30 00:56:33 -0300

    refactor: deleted redis because it is incompatible

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
 import { UploadModule } from './modules/upload/upload.module';
 import { VideoModule } from './modules/video/video.module';
 import { VideoCacheService } from './services/video-cache/video-cache.service';
@@ -13,19 +12,12 @@ import { UploadCacheService } from './services/upload-cache/upload-cache.service
   imports: [
     ConfigModule.forRoot(),
     CacheModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (config: ConfigService) => ({
-        store: await redisStore({
-          socket: {
-            host: config.get<string>('REDIS_HOST', 'redis'),
-            port: config.get<number>('REDIS_PORT', 6379),
-          },
-          ttl: config.get<number>('CACHE_TTL', 60),
-        }),
-      }),
-      inject: [ConfigService],
       isGlobal: true,
+      useFactory: () => ({
+        ttl: 60,
+      }),
     }),
+
     UploadModule,
     VideoModule
   ],
